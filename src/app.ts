044eebe46a804b9672539ac1cf9d2d1d82a2083a
Kajal <fhkajal.ouritspace@gmail.com>
2026-01-12 11:13:07 +0600

    code

--- src/app.ts
+++ src/app.ts
@@ -31,7 +31,7 @@ app.use("/images", express.static(path.join(__dirname, "../public/images")));
 app.use("/api/v1", apiRouter);
 
 app.get("/", (req: Request, res: Response) => {
-  res.send("genius tutor start!");
+  res.send("code-base start!");
 });
 
 // app.use(globalErrorHandler);
