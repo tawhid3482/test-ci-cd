75555e1cb83c15d37d81f2146dec12cbd9c12b2c
Tawhidul Islam <tawhidulislam3482@gmail.com>
2026-02-03 18:01:04 +0600

    Fix

--- src/app.ts
+++ src/app.ts
@@ -13,7 +13,7 @@ app.use(express.json());
 
 app.use(
   cors({
-    origin: ["http://localhost:3000", "http://localhost:3001"],
+    origin: ["http://localhost:3000", "http://localhost:3001","https://tr-tuition.vercel.app"],
     credentials: true,
   }),
 );
